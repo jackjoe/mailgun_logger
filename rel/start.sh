91e7595d2263d3159d39591c4023127feaaf833e
Jeroen Bourgois <jeroenbourgois@gmail.com>
2020-02-21 13:17:08 +0100

    feat: tweak startup to wait for postgres

--- rel/start.sh
+++ rel/start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# With help from https://dogsnog.blog/2018/02/02/a-docker-based-development-environment-for-elixirphoenix/
+
+set -e
+
+# Wait for Postgres to become available.
+until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
 
 ./bin/production eval "JackJoe.ReleaseTasks.migrate_and_seed"
 
