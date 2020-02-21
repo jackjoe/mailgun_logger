f0076ff94d0f7178c329ae1c37cd893cc94062c3
Jeroen Bourgois <jeroenbourgois@gmail.com>
2020-02-21 13:32:02 +0100

    fix: order of execution

--- rel/start.sh
+++ rel/start.sh
@@ -3,13 +3,13 @@
 
 set -e
 
-mix ecto.create
-
 # Wait for Postgres to become available.
 until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
+
+mix ecto.create
 
 ./bin/production eval "JackJoe.ReleaseTasks.migrate_and_seed"
 
