6f5a0c75e29ebf438c45a2dc8c775ece02a5b974
Jeroen Bourgois <jeroenbourgois@gmail.com>
2021-04-14 10:29:59 +0200

    feat: alter label colours

--- lib/mailgun_logger_web/views/event_view.ex
+++ lib/mailgun_logger_web/views/event_view.ex
@@ -12,6 +12,14 @@ defmodule MailgunLoggerWeb.EventView do
     end
   end
 
+  def event_name(event_name) when event_name in ~w(delivered) do
+    ~E"""
+    <span style="background-color: lime; padding: 3px 5px; color: green;"><%= event_name %></span>
+    """
+  end
+
+  def event_name(event_name), do: event_name
+
   def event_type(event_type) when event_type in ~w(warn) do
     ~E"""
     <span style="background-color: orange; padding: 3px 5px; color: #fff;"><%= event_type %></span>
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MailgunLogger.Mixfile do
   def project do
     [
       app: :mailgun_logger,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
