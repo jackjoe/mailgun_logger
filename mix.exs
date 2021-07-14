549917bbf62910847834809108309f8eb06e69a3
Jeroen Bourgois <jeroenbourgois@gmail.com>
2021-07-14 08:58:33 +0200

    chore: tweak styling

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MailgunLogger.Mixfile do
   def project do
     [
       app: :mailgun_logger,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
