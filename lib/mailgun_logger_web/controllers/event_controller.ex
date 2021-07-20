d9cdc14602c48a2021598d81b8ba1003927760bf
Jeroen Bourgois <jeroenbourgois@gmail.com>
2021-07-20 09:47:54 +0200

    feat: style tweaks

--- lib/mailgun_logger_web/controllers/event_controller.ex
+++ lib/mailgun_logger_web/controllers/event_controller.ex
@@ -15,11 +15,6 @@ defmodule MailgunLoggerWeb.EventController do
     )
   end
 
-  def index(conn, params) do
-    page = Events.list_events_paged(params)
-    render(conn, :index, page: page)
-  end
-
   def show(conn, %{"id" => event_id}) do
     event = Events.get_event(event_id) |> Events.preload(:account)
     render(conn, :show, event: event)
