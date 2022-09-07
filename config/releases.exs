2f6197409af85d20d301ae78a6a1d5d5e2f32f4a
Jeroen Bourgois <jeroen@jackjoe.be>
2022-09-07 14:50:26 +0200

    feat: load mailgun api key from runtime env vars

--- config/releases.exs
+++ config/releases.exs
@@ -5,3 +5,9 @@ config :mailgun_logger, MailgunLogger.Repo,
   password: System.get_env("ML_DB_PASSWORD"),
   database: System.get_env("ML_DB_NAME"),
   hostname: System.get_env("ML_DB_HOST")
+
+config :mailgun_logger, MailgunLogger.Mailer,
+  adapter: Bamboo.MailgunAdapter,
+  api_key: System.get_env("MAILGUN_API_KEY"),
+  domain: System.get_env("MAILGUN_DOMAIN"),
+  from: System.get_env("MAILGUN_FROM") || "no-reply@jackjoe.be"
