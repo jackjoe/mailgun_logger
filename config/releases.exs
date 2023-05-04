3b1b19725a0e14eab841a15973693c3e85e5853a
Jeroen Bourgois <jeroen@jackjoe.be>
2023-05-04 09:38:29 +0200

    fix: load aws vars at runtime

--- config/releases.exs
+++ config/releases.exs
@@ -11,3 +11,16 @@ config :mailgun_logger, MailgunLogger.Mailer,
   api_key: System.get_env("MAILGUN_API_KEY"),
   domain: System.get_env("MAILGUN_DOMAIN"),
   from: System.get_env("MAILGUN_FROM") || "no-reply@jackjoe.be"
+
+config :ex_aws,
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region: System.get_env("AWS_REGION"),
+  bucket: System.get_env("AWS_BUCKET"),
+  raw_path: System.get_env("RAW_PATH"),
+  s3: [
+    scheme: System.get_env("AWS_SCHEME"),
+    port: System.get_env("AWS_PORT"),
+    region: System.get_env("AWS_REGION"),
+    host: System.get_env("AWS_HOST")
+  ]
