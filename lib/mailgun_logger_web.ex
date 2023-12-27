9ac890385f3497efbda3c608068da992cee1e0f2
Jeroen Bourgois <jeroen@jackjoe.be>
2023-12-27 16:48:22 +0100

    chore: bump packages

--- lib/mailgun_logger_web.ex
+++ lib/mailgun_logger_web.ex
@@ -69,7 +69,9 @@ defmodule MailgunLoggerWeb do
   defp html_helpers do
     quote do
       # Use all HTML functionality (forms, tags, etc)
-      use Phoenix.HTML
+      import Phoenix.HTML
+      import Phoenix.HTML.Form
+      use PhoenixHTMLHelpers
       use Phoenix.LiveView
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
