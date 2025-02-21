1bbd8e6848ce26339ed445b45eda50ff4a36cbb1
Jeroen Bourgois <jeroen@jackjoe.be>
2025-02-21 13:00:36 +0100

    chore: gbump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MailgunLogger.Mixfile do
   def project do
     [
       app: :mailgun_logger,
-      version: "2406.1.0",
+      version: "2502.1.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: aliases(),
