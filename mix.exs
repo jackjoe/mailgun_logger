e1dfa5caa3b643585de2e4a95ad1bd81185c29ee
Jeroen Bourgois <jeroen@jackjoe.be>
2026-03-11 14:08:56 +0100

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MailgunLogger.Mixfile do
   def project do
     [
       app: :mailgun_logger,
-      version: "2603.2.0",
+      version: "2603.2.1",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       listeners: [Phoenix.CodeReloader],
