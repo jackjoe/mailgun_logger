74a2a04089ee5b61a9b057d184d04a7f665f25e9
Gérard Lichtert <gerard.lichtert@hotmail.com>
2026-04-16 06:29:02 +0200

    fixed a migration that caused an error due to a FK constraint from roles_users to users

--- priv/repo/migrations/20190606150819_create_roles_users.exs
+++ priv/repo/migrations/20190606150819_create_roles_users.exs
@@ -3,8 +3,8 @@ defmodule MailgunLogger.Repo.Migrations.CreateRolesUsers do
 
   def change do
     create table(:roles_users) do
-      add(:role_id, references(:roles))
-      add(:user_id, references(:users))
+      add(:role_id, references(:roles, on_delete: :delete_all), null: false)
+      add(:user_id, references(:users, on_delete: :delete_all), null: false)
 
       timestamps()
     end
